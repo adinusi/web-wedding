497cb2769c81a7f333870441b83af826c1832fce
adytia28 <itdev.adytia@gmail.com>
2025-06-06 20:44:28 +0700

    Update: pagination button, copy button adress, and clear input name

--- theme.js
+++ theme.js
@@ -154,7 +154,7 @@
       for (var _i2 = 0; _i2 < countdownElement.length; _i2++) {
         displayCountdown(countdownElement[_i2]);
       }
-      var copyText = function copyText(e) {
+      var copyText = function copyText(e, type = 'bank') {
         var newInp = document.createElement("input");
         newInp.autofocus = false;
         
@@ -171,10 +171,17 @@
         var oldText = target.innerHTML;
     
         // Ikon centang hijau
-        var checkIcon = `
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#9EB996" class="size-6" style="width: 22px; margin-left: 5px;">
-            <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clip-rule="evenodd" />
-          </svg>`;
+        var checkIcon;
+
+        if(type == 'bank') {
+          checkIcon = `
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#9EB996" class="size-6" style="width: 22px; margin-left: 5px;">
+              <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clip-rule="evenodd" />
+            </svg>`;
+        } else {
+          checkIcon = `
+           Copied`;
+        }
 
         target.innerHTML = `${checkIcon}`;
     
